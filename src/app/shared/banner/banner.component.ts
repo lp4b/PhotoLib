99a7b5064cc0270c04d9dcd5c17674ddbe1856d3
lewoos <lewoos@ya.ru>
2020-04-01 20:36:52 +0300

    поправил просмотр детального фото

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -3,10 +3,13 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
   @Input() imgUrl = '/assets/img/angular-banner.jpg';
+  @Input() maxHeight = 'none';
+  @Input() title;
+  @Input() author;
 
   constructor() {
   }
